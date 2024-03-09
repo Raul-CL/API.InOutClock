2c922c5d04e18495f958b88954cb3bc932d55dec
PC <PC@DESKTOP-2TQ5VIT>
2024-03-09 13:59:42 -0600

    Add project files.

---
+++ API.InOutClock.Shared/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.InOutClock.Shared
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string PayrollId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string NormalizedName { get; set; }
+        public int ShiftId { get; set; }
+        public int DepartmentId { get; set; }
+        
+    }
+}
---
+++ API.InOutClock.Shared/Shift.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.InOutClock.Shared
+{
+    internal class Shift
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string NormalizedDescription { get; set; }
+        public DateTime In { get; set; }
+        public DateTime Out { get; set; }
+    }
+}
