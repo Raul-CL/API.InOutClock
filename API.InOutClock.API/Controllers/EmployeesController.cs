418bc146b75dcc302b4f1555937d4f4cf3ebb5e9
Raul Alberto Corral Lara <raulcorral20@gmail.com>
2024-03-10 01:42:55 -0600

    Employees Controller Created

---
+++ API.InOutClock.API/Controllers/EmployeesController.cs
@@ -0,0 +1,78 @@
+﻿using API.InOutClock.Data;
+using API.InOutClock.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.InOutClock.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly APIInOutClockContext _context;
+
+        public EmployeesController(APIInOutClockContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        [HttpGet("{payrollId}")]
+        public async Task<ActionResult<Employee>> GetEmployeeByPayrollId(string payrollId)
+        {
+            var employee = await _context.Employees.SingleOrDefaultAsync(emp => emp.PayrollId == payrollId);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        [HttpGet("{name}")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployeeByName(string name)
+        {
+            var employees = await _context.Employees.Where(emp => emp.NormalizedName.Contains(name)).ToListAsync();
+
+            if (employees == null || employees.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return employees;
+        }
+
+        [HttpGet("department/{departmentId}")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployeesByDepartment(int departmentId)
+        {
+            var employees = await _context.Employees.Where(emp => emp.DepartmentId == departmentId).ToListAsync();
+            
+            if (employees == null)
+            {
+                   return NotFound();
+            }
+
+            return employees;
+        }
+
+        [HttpGet("shift/{shiftId}")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployeesByShift(int shiftId)
+        {
+            var employees = await _context.Employees.Where(emp => emp.ShiftId == shiftId).ToListAsync();
+            
+            if (employees == null)
+            {
+                   return NotFound();
+            }
+
+            return employees;
+        }
+    }
+}
---
+++ API.InOutClock.Data/Migrations/20240310030134_Initial.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.InOutClock.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Checks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    Record = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TypeOfCheck = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    ShiftId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NormalizedDescription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PayrollId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NormalizedName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShiftId = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shifts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NormalizedDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    In = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Out = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shifts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Checks");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Shifts");
+        }
+    }
+}
