0db6a2b74e2ef3b611e35942942e67387fe8ba54
Raul Alberto Corral Lara <raulcorral20@gmail.com>
2024-03-12 08:17:17 -0600

    ShiftController Created

---
+++ API.InOutClock.API/Controllers/ShiftController.cs
@@ -0,0 +1,102 @@
+﻿using API.InOutClock.Data;
+using API.InOutClock.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.InOutClock.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShiftController : ControllerBase
+    {
+        private readonly APIInOutClockContext _context;
+
+        public ShiftController(APIInOutClockContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Shift>>> GetShifts()
+        {
+            return Ok(await _context.Shifts.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shift>> GetShift(int id)
+        {
+            var shift = await _context.Shifts.SingleOrDefaultAsync(dep => dep.Id == id);
+
+            if (shift == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(shift);
+        }
+
+        [HttpGet("description/{description}")]
+        public async Task<ActionResult<IEnumerable<Shift>>> GetShiftByDescription(string description)
+        {
+            var shifts = await _context.Shifts.Where(dep => dep.NormalizedDescription.Contains(description)).ToListAsync();
+
+            if (shifts == null || shifts.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(shifts);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Shift>> PostShift(Shift shift)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Shifts.Add(shift);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetShift", new { id = shift.Id }, shift);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShift(int id, Shift shift)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (await _context.Shifts.AnyAsync(shi => shi.Id == shift.Id))
+            {
+                _context.Shifts.Update(shift);
+                await _context.SaveChangesAsync();
+                return Ok(shift);
+            }
+            else
+            {
+                return NotFound("El turno no existe");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Shift>> DeleteShift(int id)
+        {
+            var shift = await _context.Shifts.SingleOrDefaultAsync(dep => dep.Id == id);
+
+            if (shift == null)
+            {
+                return NotFound();
+            }
+
+            _context.Shifts.Remove(shift);
+            await _context.SaveChangesAsync();
+
+            return Ok(shift);
+        }
+    }
+}
