1669fdc30775f6dea8aab1df23349d568f3653ee
Raul Alberto Corral Lara <raulcorral20@gmail.com>
2024-03-14 00:36:10 -0600

    change models by normalized description update database

--- API.InOutClock.Data/Migrations/20240314063507_NormalizedDescription changed.Designer.cs
+++ API.InOutClock.Data/Migrations/20240314063507_NormalizedDescription changed.Designer.cs
@@ -4,6 +4,7 @@ using API.InOutClock.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.InOutClock.Data.Migrations
 {
     [DbContext(typeof(APIInOutClockContext))]
-    partial class APIInOutClockContextModelSnapshot : ModelSnapshot
+    [Migration("20240314063507_NormalizedDescription changed")]
+    partial class NormalizedDescriptionchanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API.InOutClock.Data/Migrations/20240314063507_NormalizedDescription changed.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.InOutClock.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NormalizedDescriptionchanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PayrollId",
+                table: "Employees",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PayrollId",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedName",
+                table: "Employees",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
