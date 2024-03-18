8ae4287c3216029483565f53c9b0638b4334fae6
Raul Alberto Corral Lara <raulcorral20@gmail.com>
2024-03-18 14:50:32 -0600

    Implementation JWT Fist Part

--- API.InOutClock.API/Program.cs
+++ API.InOutClock.API/Program.cs
@@ -1,5 +1,8 @@
 using API.InOutClock.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+//Autenticacion con JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {   options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            //ValidIssuer = "http://localhost:5000",
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            //ValidAudience = "http://localhost:5000",
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<APIInOutClockContext>(options =>
@@ -23,7 +47,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+//JWT Importante el orden
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
