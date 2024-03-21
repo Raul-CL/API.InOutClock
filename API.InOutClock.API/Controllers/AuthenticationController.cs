fe1228edf76391ef3df83cb6de09bbf55b48cd25
Raul Alberto Corral Lara <raulcorral20@gmail.com>
2024-03-21 02:26:49 -0600

    AuthResult, DTO and AuthenticationController Created

---
+++ API.InOutClock.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using API.InOutClock.API.Configurations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace API.InOutClock.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtConfig _jwtConfig;
+
+        public AuthenticationController(UserManager<IdentityUser> userManager, IOptions<JwtConfig> jwtConfig)
+        {
+            _userManager = userManager;
+            _jwtConfig = jwtConfig.Value;
+        }
+    }
+}
---
+++ API.InOutClock.Shared/Auth/AuthResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.InOutClock.Shared.Auth
+{
+    public class AuthResult
+    {
+        public string Token { get; set; }
+        public bool Success { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
